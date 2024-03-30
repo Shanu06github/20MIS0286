baee6216e9236dc8ba938bafee4069bf2ddb22aa
Shanmuga Priyan T <138189691+Shanu06github@users.noreply.github.com>
2024-03-30 17:23:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,205 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='design'>
+      <h1 align="center">TOP N PRODUCTS</h1>
+      <table>
+        <tr>
+          <th>productName:</th>
+          <td>Laptop 1,</td>
+        </tr>
+        <tr>
+          <th align="left">price:</th>
+          <td>2236,</td>
+        </tr>
+        <tr>
+          <th align="left">rating:</th>
+          <td>4.7,</td>
+        </tr>
+        <tr>
+          <th align="left">discount:</th>
+          <td>63,</td>
+        </tr>
+        <tr>
+          <th align="left">availablity</th>
+          <td>yes</td>
+        </tr><br></br>
+
+        <tr>
+          <th>productName:</th>
+          <td>Laptop 13,</td>
+        </tr>
+        <tr>
+          <th align="left">price:</th>
+          <td>1244,</td>
+        </tr>
+        <tr>
+          <th align="left">rating:</th>
+          <td>4.5,</td>
+        </tr>
+        <tr>
+          <th align="left">discount:</th>
+          <td>45,</td>
+        </tr>
+        <tr>
+          <th align="left">availablity</th>
+          <td>out of stock</td>
+        </tr><br></br>
+
+        <tr>
+          <th>productName:</th>
+          <td>Laptop 3,</td>
+        </tr>
+        <tr>
+          <th align="left">price:</th>
+          <td>9102,</td>
+        </tr>
+        <tr>
+          <th align="left">rating:</th>
+          <td>4.44,</td>
+        </tr>
+        <tr>
+          <th align="left">discount:</th>
+          <td>98,</td>
+        </tr>
+        <tr>
+          <th align="left">availablity</th>
+          <td>out of stock</td>
+        </tr><br></br>
+
+        <tr>
+          <th>productName:</th>
+          <td>Laptop 11,</td>
+        </tr>
+        <tr>
+          <th align="left">price:</th>
+          <td>2652,</td>
+        </tr>
+        <tr>
+          <th align="left">rating:</th>
+          <td>4.12,</td>
+        </tr>
+        <tr>
+          <th align="left">discount:</th>
+          <td>70,</td>
+        </tr>
+        <tr>
+          <th align="left">availablity</th>
+          <td>out of stock</td>
+        </tr><br></br>
+
+        <tr>
+          <th>productName:</th>
+          <td>Laptop 4,</td>
+        </tr>
+        <tr>
+          <th align="left">price:</th>
+          <td>1258,</td>
+        </tr>
+        <tr>
+          <th align="left">rating:</th>
+          <td>3.8,</td>
+        </tr>
+        <tr>
+          <th align="left">discount:</th>
+          <td>33,</td>
+        </tr>
+        <tr>
+          <th align="left">availablity</th>
+          <td>yes</td>
+        </tr><br></br>
+
+        <tr>
+          <th>productName:</th>
+          <td>Laptop 14,</td>
+        </tr>
+        <tr>
+          <th align="left">price:</th>
+          <td>9254,</td>
+        </tr>
+        <tr>
+          <th align="left">rating:</th>
+          <td>3,</td>
+        </tr>
+        <tr>
+          <th align="left">discount:</th>
+          <td>56,</td>
+        </tr>
+        <tr>
+          <th align="left">availablity</th>
+          <td>yes</td>
+        </tr><br></br>
+
+        <tr>
+          <th>productName:</th>
+          <td>Laptop 1,</td>
+        </tr>
+        <tr>
+          <th align="left">price:</th>
+          <td>1059,</td>
+        </tr>
+        <tr>
+          <th align="left">rating:</th>
+          <td>2.77,</td>
+        </tr>
+        <tr>
+          <th align="left">discount:</th>
+          <td>21,</td>
+        </tr>
+        <tr>
+          <th align="left">availablity</th>
+          <td>yes</td>
+        </tr><br></br>
+
+        <tr>
+          <th>productName:</th>
+          <td>Laptop 10,</td>
+        </tr>
+        <tr>
+          <th align="left">price:</th>
+          <td>7145,</td>
+        </tr>
+        <tr>
+          <th align="left">rating:</th>
+          <td>2.74,</td>
+        </tr>
+        <tr>
+          <th align="left">discount:</th>
+          <td>15,</td>
+        </tr>
+        <tr>
+          <th align="left">availablity</th>
+          <td>yes</td>
+        </tr><br></br>
+
+        <tr>
+          <th>productName:</th>
+          <td>Laptop 10,</td>
+        </tr>
+        <tr>
+          <th align="left">price:</th>
+          <td>4101,</td>
+        </tr>
+        <tr>
+          <th align="left">rating:</th>
+          <td>2.67,</td>
+        </tr>
+        <tr>
+          <th align="left">discount:</th>
+          <td>37,</td>
+        </tr>
+        <tr>
+          <th align="left">availablity</th>
+          <td>out of stock</td>
+        </tr><br></br>
+        
+      </table>
+     
+    </div>
+    
+  );
+};
+
+export default App;
